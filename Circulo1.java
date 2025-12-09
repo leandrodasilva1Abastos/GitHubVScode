d76636adccf9823aa7a2c0b988d4746bb68a4760
Leandro <leadasros@alu.edu.gva.es>
2025-12-09 18:44:01 +0100

    Creado en VScode

---
+++ Circulo1.java
@@ -0,0 +1,26 @@
+public class Circulo1 {
+
+    private double centroX;
+    private double centroY;
+    private double radio;
+
+    public Circulo1(double x, double y, double r) {
+        centroX = x;
+        centroY = y;
+        radio = r;
+    }
+
+    public double getCentroX() {
+        return centroX;
+    }
+
+    public double getPerimetro() {
+        return 2 * Math.PI * radio;
+    }
+
+    public void mueve(double despX, double despY) {
+        centroX = centroX + despX;
+        centroY = centroY + despY;
+    }
+}
+
