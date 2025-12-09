7555d7eea43f42c16797886788555f598dd3d134
Leandro <leadasros@alu.edu.gva.es>
2025-12-09 18:47:07 +0100

    Segundo commit en VScode

---
+++ Empleado1.java
@@ -0,0 +1,45 @@
+/*
+ Ejercicio de Documentación
+ * Empleados - Versión sin documentación
+ */
+
+public class Empleado1 {
+// prueba 9/12
+ // prueba pull
+    //Atributos
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private double salario;
+
+    //Metodos publicos
+    public boolean plus(double sueldoPlus) {
+        boolean aumento = false;
+        if (edad > 40 && compruebaNombre()) {
+            salario += sueldoPlus;
+            aumento = true;
+        }
+        return aumento;
+    }
+
+    //Metodos privados
+    private boolean compruebaNombre() {
+        if (nombre.equals("")) {
+            return false;
+        }
+        return true;
+    }
+
+    //Constructores
+    public Empleado1() {
+        this("", "", 0, 0);
+    }
+
+    public Empleado1(String nombre, String apellido, int edad, double salario) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.salario = salario;
+    }
+}
+
